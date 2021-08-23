6a41859ea322a0f590a3173cf8887af29af10cbe
Saurabh Sharma <saurabhshcs@gmail.com>
2021-08-23 02:00:10 +0100

    [TSZ-001] Spring-integration-example saurabhshcs - Add initial code setup

---
+++ src/main/java/com/techsharezone/spring/integration/entity/Demo.java
@@ -0,0 +1,10 @@
+package com.techsharezone.spring.integration.entity;
+
+/*
+ * @created 23/08/2021 -01:59
+ * @project spring-integration-example
+ * @author  saurabhshcs
+ */
+
+public class Demo {
+}
